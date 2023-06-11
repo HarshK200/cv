f7d7862e573dba645c1b0a0a42ed167242d07a39
HarshK200 <harshkapadia04@gmail.com>
2023-06-11 22:01:43 +0530

    finally fixed the dynamic date problem

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const date = new Date()
 const date_element = document.getElementById("dynamic_date")
 const year = date.getFullYear();
 
-date_element.innerText(`© ${year} Harsh K.@Sharkster youtube`)
+console.log(year)
+
+date_element.innerHTML = `© ${year} Harsh K.@Sharkster youtube`

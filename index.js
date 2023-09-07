9840134e26e0cfb449c27779106bf171afda522b
Harsh Kapadia <110605244+HarshK200@users.noreply.github.com>
2023-09-07 19:32:15 +0530

    removed the console.log(year)

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const date = new Date()
 const date_element = document.getElementById("dynamic_date")
 const year = date.getFullYear();
 
-console.log(year)
+// console.log(year)
 
 date_element.innerHTML = `© ${year} Harsh K.@Sharkster youtube`

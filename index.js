b9a34e7d4970775474d06f547d6077829f708b30
HarshK200 <harshkapadia04@gmail.com>
2024-03-06 19:48:55 +0530

    copyright changes & twitter link updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const date = new Date()
-const date_element = document.getElementById("dynamic_date")
+const date = new Date();
+const date_element = document.getElementById("dynamic_date");
 const year = date.getFullYear();
 
 // console.log(year)
 
-date_element.innerHTML = `© ${year} Harsh K.@Sharkster youtube`
+date_element.innerHTML = `© ${year} Harsh Kapadia`;
